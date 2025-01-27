b471545903dbe1e81b88c0ed40e958933357ae7f
castilloxavie <xcastillovaron93@gmail.com>
2025-01-26 23:58:09 -0500

    UiMenu

--- src/main/java/Doctor.java
+++ src/main/java/Doctor.java
@@ -1,10 +1,23 @@
 public class Doctor {
-    int id;
+    static int id = 0;
     String name;
     String speciality;
 
     //comportamientos
+    Doctor(){
+        System.out.println("construyendo la instancia de Doctor");
+        id++;
+    }
+
+    Doctor(String name){
+        System.out.println("el nombre del doctor es: " + name);
+    }
+
     public void showName(){
         System.out.println(name);
+    }
+
+    public  void  showId(){
+        System.out.println("Id del doctor es: "+id);
     }
 }
